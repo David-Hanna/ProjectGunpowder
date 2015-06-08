12c068f57c898a12e99c36b1403a01d19caead83
David Hanna <dhanna@upei.ca>
2015-06-08 20:36:04 -0300

    NoFriction physics materials + smooth camera movement, limits
    Ditched generic movement script. Movement should be customized to each
    object.

---
+++ Unity/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+	
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		float horizontal = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+		float vertical = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+
+		rigidbody.velocity = new Vector3 (horizontal, vertical, 0.0f);
+		//ownerTransform.Translate (new Vector3 (horizontal, vertical, 0.0f));
+
+	}
+}
