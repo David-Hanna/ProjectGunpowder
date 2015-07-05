ee7ecdc74cd7f587c6885862fb69b6366535e403
Adam LeBlanc <leblanca37@gmail.com>
2015-07-04 23:29:48 -0300

    Added .cs files for the Event Management System, which willbe responsible for dealing with events that happen during the level. As well as a LoadLevel class which will read in the xml when the level is loaded.

---
+++ Unity/Assets/Scripts/EventManager.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * 	EventManager.cs
+ * 
+ *	Type Safe Event System for Unity Game Engine. 
+ *	
+ *	Author: Adam LeBlanc
+ *	Author's Email: leblanca37@gmail.com
+ *
+ *	Version: 0.1
+ *	Last Editited: 7/4/2015
+ */
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Extend this class to create custome event types, and to pass parameters with the events/
+ */
+public class GameEvent
+{
+
+}
+
+public class EventManager : MonoBehaviour 
+{
+	
+
+}
