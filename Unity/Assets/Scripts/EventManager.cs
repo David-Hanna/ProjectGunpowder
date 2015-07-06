09396b8a2333afb239155c8cca1e5b22eb4c9370
Adam LeBlanc <leblanca37@gmail.com>
2015-07-06 16:38:53 -0300

    Finished initial version of the EventManager script. More details are included in the header comment.

--- Unity/Assets/Scripts/EventManager.cs
+++ Unity/Assets/Scripts/EventManager.cs
@@ -6,11 +6,36 @@
  *	Author: Adam LeBlanc
  *	Author's Email: leblanca37@gmail.com
  *
- *	Version: 0.1
+ *	Version: 0.1.2
  *	Last Editited: 7/4/2015
  */
 
-    //TODO : Add in ussage examples in a long comment
+
+    /*
+        *************HOW TO USE***************
+        - Add this script to an empty gameobject. This gameobject will not be destroyed when a new level is loaded.
+        - Add a reference to this gameobject in any script you wish to listen for or raise events.
+        - Create your own event types by extending the GameEvent class.
+        - Create a Method that conforms to the GameEventDelegate
+            - Any method you wish to be called when an event is raised musy confrom to the GameEventDelegate
+                - This means that the one and only parameter the method takes must be a type which extends GameEvent
+                - In the event where GameEvent type B extends A, and Event A is raised, methods listening for B will not recive the callback.
+        - Create a delegate that points to the method you with to have called when the type of event it takes as a parameter is raised
+        - Add it to the event manager with the AddSubscriber method.
+            - The First parameter is required, and is the delegate.
+            - The second parameter is optional, and if true, will make it so the subscriber will still be notifed when an event is raised,
+                even if a new level if loaded. It is up to you to make sure the gameobject the script that the delegate belongs to is not destroyed when
+                a new level is loaded.
+        - To remove an event use the RemoveSubscriber method. You can simply create a new delegate that points to the same method you added previously.
+            This will also remove delegates which have been marked as persistant
+        - To raise an event. Create a new instance of the type of event you want to raise, and set all fields in the event that meed to be set. Then pass it to the
+            Raise method. This will notify all delegates which have subscribed to that event. 
+
+    ***NOTE***
+    - If you more than one delegate that points to the same method (or the same delegate more than once), that method will be called that many times when an event is raised.
+    - If you do the above, and try to remove them, it will only remove one. So you will have to call RemoveSubscriber once for each time you added it.
+    - Functionality, and API is still subject to change as this is an early version. Use at your own risk.
+    */
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -55,7 +80,11 @@ public class EventManager : MonoBehaviour
 
     void OnLevelWasLoaded()
     {
-        //TODO: Code to clone persistentDelegates and assign the clone to delegates
+        delegates = new Dictionary<Type, Delegate>();
+        foreach (KeyValuePair<Type, Delegate> kv in persistentDelegates)
+        {
+            delegates.Add(kv.Key, kv.Value);
+        }
     }
 
     public void AddSubscriber<T>(GameEventDelegate<T> listener, bool persistent = false) where T : GameEvent
@@ -71,9 +100,17 @@ public class EventManager : MonoBehaviour
             delegates[typeof(T)] = listener;
         }
 
-       //TODO : Add in code for persistent delegates
-       //If presistent == true
-       //add delegate to presistentDelegates using same logic as above
+        if (persistent)
+        {
+            if (persistentDelegates.TryGetValue(typeof(T), out tempdel))
+            {
+                persistentDelegates[typeof(T)] = Delegate.Combine(tempdel, listener);
+            }
+            else
+            {
+                persistentDelegates[typeof(T)] = listener;
+            }
+        }
 
 	}
 
@@ -82,7 +119,6 @@ public class EventManager : MonoBehaviour
         Delegate tempdel;
         if (delegates.TryGetValue(typeof(T), out tempdel))
         {
-            Debug.Log("Removed an Event");
             Delegate curDel = Delegate.Remove(tempdel, listener);
 
             if (curDel == null)
@@ -94,8 +130,20 @@ public class EventManager : MonoBehaviour
                 delegates[typeof(T)] = curDel;
             }
         }
-        //TODO : Add in code for persistent delegates
-        //same logic as above
+
+        if (persistentDelegates.TryGetValue(typeof(T), out tempdel))
+        {
+            Delegate curDel = Delegate.Remove(tempdel, listener);
+
+            if (curDel == null)
+            {
+                persistentDelegates.Remove(typeof(T));
+            }
+            else
+            {
+                persistentDelegates[typeof(T)] = curDel;
+            }
+        }
     }
 
     public void Raise<T>(T e) where T : GameEvent
--- Unity/Assets/Scripts/Test/EventTest.cs
+++ Unity/Assets/Scripts/Test/EventTest.cs
@@ -7,27 +7,44 @@ public class EventTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        GameEventDelegate<TimeEvent> ge = TimeListener;
+        GameEventDelegate<TimeEvent> oneOff = OneOffTime;
 
-        EventManager.Instance.AddSubscriber(ge);
-	}
+        EventManager.Instance.AddSubscriber(oneOff);
+
+        GameEventDelegate<TimeEvent> persistent = PersistentTime;
+
+        EventManager.Instance.AddSubscriber(persistent, true);
+    }
 
     void Update()
     {
         timeSinceStart += Time.deltaTime;
 
-        if (timeSinceStart >= 5)
+        if (timeSinceStart > 5)
             EventManager.Instance.Raise(new TimeEvent(timeSinceStart));
     }
 
-    private void TimeListener(TimeEvent e)
+    private void OneOffTime(TimeEvent e)
     {
-        Debug.Log(e.time);
-        GameEventDelegate<TimeEvent> ge = TimeListener;
+        Debug.Log(string.Format("One Off time Event, {0}", e.time));
+        //GameEventDelegate<TimeEvent> ge = OneOffTime;
 
-        EventManager.Instance.RemoveSubscriber(ge);
+        //EventManager.Instance.RemoveSubscriber(ge);
+        Application.LoadLevel("EventManagerTestSecond");
     }
-	
+
+    private void PersistentTime(TimeEvent e)
+    {
+        Debug.Log(string.Format("Presistant time Event"));
+
+        if (e.time >= 20)
+        {
+            GameEventDelegate<TimeEvent> ge = PersistentTime;
+
+            EventManager.Instance.RemoveSubscriber(ge);
+        }
+    }
+
 }
 
 public class TimeEvent : GameEvent
